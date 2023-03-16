ac49c6e1ff74f28d6e3da6b3890a22290179a7f0
WarriorDeveloper <seba.iron28@gmail.com>
2023-03-16 19:12:30 -0300

    postClient router

--- api/src/controllers/clients/postClient.js
+++ api/src/controllers/clients/postClient.js
@@ -1,9 +1,102 @@
 const { client, activities } = require("../../db")
 
+/*const postClient = async (req, res) => {
+    try {
+        const {
+            user,
+            name,
+            dni,
+            avatar,
+            height,
+            weight,
+            age,
+            phone, 
+            mail,
+            adress,
+            activitie,
+            routine
+        } = req.body
 
+        const active = true
+        const debt = false
+        const debtAmount = 0
 
-const postClient = async (req, res) => {
-    
+        const newClient = await client.create({
+            user,
+            name,
+            dni,
+            avatar,
+            height,
+            weight,
+            age,
+            phone, 
+            mail,
+            adress,
+            routine,
+            active,
+            debt,
+            debtAmount
+        })
+
+        const Activities = await activities.findAll({
+            where: {
+                name : activitie
+            }
+        })
+        await newClient.addactivities(Activities)/
+
+        res.status(200).send(newClient)
+
+    } catch (error) {
+        res.status(404).json({error:error.message})
+    }
+}
+
+module.exports = postClient
+*/
+
+const postClient = async ({ user,
+    name,
+    dni,
+    avatar,
+    height,
+    weight,
+    age,
+    phone,
+    mail,
+    adress,
+    // activitie,
+    routine }) => {
+    const active = true
+    const debt = false
+    const debtAmount = 0
+
+    const newClient = await client.create({
+        user,
+        name,
+        dni,
+        avatar,
+        height,
+        weight,
+        age,
+        phone,
+        mail,
+        adress,
+        routine,
+        active,
+        debt,
+        debtAmount
+    })
+    /*
+    const Activities = await activities.findAll({
+            where: {
+                name : activitie
+            }
+        })
+        await newClient.addactivities(Activities)/
+    */
+
+    return newClient
 }
 
 module.exports = postClient
--- api/src/routes/clients.router.js
+++ api/src/routes/clients.router.js
@@ -19,10 +19,17 @@ DELETE - deactivate client for id
 PUT - update client for id
 */
 
-//router.get('/', (req, res)=>{})
+router.get('/', (req, res)=>{}) 
 //router.get('/:id', (req, res)=>{})
 
-router.post('/', postClient)
+router.post('/', async(req, res)=>{
+    try {
+        const newClient = await postClient(req.body)
+        res.status(200).json(newClient)
+    } catch (error) {
+        res.status(404).json({error:error.message})
+    }
+})
 
 router.delete('/:id', deleteClient)
 
