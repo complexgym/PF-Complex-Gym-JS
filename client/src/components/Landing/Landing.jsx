f8bcf16affc723919cac006738543a5f9dd07e4d
MrTban <esteman214@gmail.com>
2023-03-17 14:55:25 -0500

    link login btton with auth0

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,40 +1,46 @@
-import Spline from "@splinetool/react-spline";
-import style from "./style.module.css";
-import { Link } from "react-router-dom";
-import React from "react";
+import Spline from '@splinetool/react-spline';
+import style from './style.module.css';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Landing2() {
-  return (
-    <div className={style.container}>
-      <div className={style.waves_container}>
-        <Spline
-          className={style.splinewaves}
-          scene="https://prod.spline.design/ETlKk7yNQfbifC5m/scene.splinecode"
-        />
-      </div>
-      <div className={style.left_container}>
-        <h1>El 90% del éxito se basa en el esfuerzo.</h1>
-        <div className={style.buttons}>
-          <Link to="/home">
-            <button type="button" className={style.buttonn}>
-              EXPLORAR
-            </button>
-          </Link>
-          <Link to="/login">
-            <button type="button" className={style.buttonn}>
-              INICIAR SESION
-            </button>
-          </Link>
-        </div>
-      </div>
-      <div className={style.gym_container}>
-        <div className={style.spline_gym_div}>
-          <Spline
-            className={style.gym_spline}
-            scene="https://prod.spline.design/8ZGu1Nn72DALpfwr/scene.splinecode"
-          />
-        </div>
-      </div>
-    </div>
-  );
+	const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+	return (
+		<div className={style.container}>
+			<div className={style.waves_container}>
+				<Spline
+					className={style.splinewaves}
+					scene='https://prod.spline.design/ETlKk7yNQfbifC5m/scene.splinecode'
+				/>
+			</div>
+			<div className={style.left_container}>
+				<h1>El 90% del éxito se basa en el esfuerzo.</h1>
+				<div className={style.buttons}>
+					<Link to='/home'>
+						<button type='button' className={style.buttonn}>
+							EXPLORAR
+						</button>
+					</Link>
+					{!isAuthenticated && (
+						<button className={style.buttonn} onClick={() => loginWithRedirect()}>
+							INICIAR SESIÓN
+						</button>
+					)}
+					{/* <Link to='/login'>
+						<button type='button'>INICIAR SESION</button>
+					</Link> */}
+				</div>
+			</div>
+			<div className={style.gym_container}>
+				<div className={style.spline_gym_div}>
+					<Spline
+						className={style.gym_spline}
+						scene='https://prod.spline.design/8ZGu1Nn72DALpfwr/scene.splinecode'
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
