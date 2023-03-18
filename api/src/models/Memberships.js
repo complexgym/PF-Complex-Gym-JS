1935a829572065c55f7437663c8c344c7ad88dd9
Yosemar <yojecava@outlook.com>
2023-03-18 15:59:14 -0600

    "model memberships"

--- api/src/models/Memberships.js
+++ api/src/models/Memberships.js
@@ -2,9 +2,33 @@ const { DataTypes } = require("sequelize");
 
 const memberships = (sequelize) => {
     sequelize.define('memberships', {   
-        authorId: {
+        clientid:{
+            type: DataTypes.UUID,
+			allowNull: true,
+        },
+        planName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+        actividadId: {
+            type: DataTypes.UUID,
+			allowNull: true,
+        },
+        Price: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+			allowNull: true,
+        },
+        startDate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        finishDate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        paymentId: {
+            type: DataTypes.UUID,
+			allowNull: true,
         },
      }, {timestamps: false})
 }
