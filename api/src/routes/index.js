c72fc722d927c5fd24d4fcc0b5a28cbdbae50610
AAlejof <scaramouch11@gmail.com>
2023-03-18 21:07:56 -0300

    add trainer route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,9 +4,11 @@ const router = Router()
 const clients = require('./clients.router')
 const activities = require('./activities.router')
 const publications = require('./publications.router')
+const trainer = require('./trainer.router')
 
 router.use('/clients', clients)
 router.use('/activities', activities)
 router.use('/publications', publications)
+router.use('./trainer', trainer)
 
 module.exports = router
