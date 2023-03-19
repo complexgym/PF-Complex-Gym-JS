cd0edcd3ac732cefedebef9a18774541aa409741
MrTban <esteman214@gmail.com>
2023-03-18 20:16:41 -0500

    updated NavBar styles

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginBtn from '../LoginBtn/LoginBtn';
 import LogoutBtn from '../LogoutBtn/LogoutBtn';
-import Spline from '@splinetool/react-spline';
+// import Spline from '@splinetool/react-spline';
+import logo from '../../assets/logo/logo.png';
 
 export default function Navbar() {
 	const { user, isAuthenticated } = useAuth0();
@@ -12,14 +13,17 @@ export default function Navbar() {
 	return (
 		<div className=' fixed z-20 flex flex-row w-screen text-white py-2 bg-[#231f20] bg-opacity-80 items-center'>
 			<div className='flex flex-row w-[90%] mx-[5%] text-white pb-2 items-center border-y-2 border-white border-opacity-20'>
-				<div className=' h-16 pl-[10%] justify-center'>
-					<Link to='/'>
+				<div className=''>
+					{/* <Link to='/'>
 						<Spline scene='https://prod.spline.design/ffORQphusIoT5k1H/scene.splinecode' />
+					</Link> */}
+					<Link to='/'>
+						<img className='w-[10%] ml-[10%] -mb-2 ' src={logo} alt='logo' />
 					</Link>
 				</div>
 				<br />
-				<div className='flex  w-screen mx-[5%] justify-end py-1 '>
-					<div className='flex  gap-12 py-3'>
+				<div className='flex  w-screen mr-[5%] justify-end '>
+					<div className='flex  gap-12 items-center'>
 						<Link to='/home'>Inicio</Link>
 
 						<Link to='/nosotros'>Nosotros</Link>
