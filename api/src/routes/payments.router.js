80979eadec01f6e6e937fe704d99fdd64e815895
Yosemar <yojecava@outlook.com>
2023-03-22 06:13:03 -0600

    mercado pago

--- api/src/routes/payments.router.js
+++ api/src/routes/payments.router.js
@@ -1,10 +1,6 @@
 const { Router } = require('express')
 const router = Router()
-const mercadopago = require("mercadopago");
 
-mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN_MP,
-  });
 const {postPayments, getFeedback} = require('../controllers/index')
 
 //router.get('/', (req, res)=>{})
