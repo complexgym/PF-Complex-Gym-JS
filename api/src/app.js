d9493c9ae4ec4f6f14f124b239a6aeff28e54c23
MrTban <esteman214@gmail.com>
2023-03-23 23:23:13 -0500

    local

--- api/src/app.js
+++ api/src/app.js
@@ -9,13 +9,13 @@ const server = express();
 
 server.name = 'API';
 
-
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'https://pf-complex-gym-henry.vercel.app/'); // ?????????????????
+	// res.header('Access-Control-Allow-Origin', 'https://pf-complex-gym-henry.vercel.app/'); // ?????????????????
+	res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // ?????????????????
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header(
 		'Access-Control-Allow-Headers',
