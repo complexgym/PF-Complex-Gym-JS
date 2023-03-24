2f6790f4f991810aa6374bd0ec64d056c73e7a24
nicovon24 <nicovon24@gmail.com>
2023-03-24 11:08:43 -0300

    res.header * in api json

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(express.json());
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'https://pf-complex-gym-henry.vercel.app/'); // ?????????????????
+	res.header('Access-Control-Allow-Origin', '*'); // ?????????????????
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header(
 		'Access-Control-Allow-Headers',
