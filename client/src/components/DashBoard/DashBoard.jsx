0bf6bf251a7ebb88a43126dd844c977785c12150
MrTban <esteman214@gmail.com>
2023-03-24 19:59:46 -0500

    styles dash updated and create route/calendar

--- client/src/components/DashBoard/DashBoard.jsx
+++ client/src/components/DashBoard/DashBoard.jsx
@@ -12,19 +12,13 @@ const DashBoard = () => {
 
 				<main className='relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl'>
 					{/* <!-- Navbar --> */}
-					<nav
-						className='relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start'
-						navbar-main
-						navbar-scroll='false'
-					>
+					<nav className='relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start'>
 						<div className='flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit'>
 							<nav>
 								{/* <!-- breadcrumb --> */}
 								<ol className='flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16'>
 									<li className='text-sm leading-normal'>
-										<a className='text-white opacity-50' href='javascript:;'>
-											Paginas
-										</a>
+										<a className='text-white opacity-50'>Paginas</a>
 									</li>
 									<li
 										className="text-sm pl-2 capitalize leading-normal text-white before:float-left before:pr-2 before:text-white before:content-['/']"
@@ -49,30 +43,6 @@ const DashBoard = () => {
 										/>
 									</div>
 								</div>
-								<ul className='flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full'>
-									<li className='flex items-center pl-4 xl:hidden'>
-										<a
-											href='javascript:;'
-											className='block p-0 text-sm text-white transition-all ease-nav-brand'
-											sidenav-trigger
-										>
-											<div className='w-4.5 overflow-hidden'>
-												<i className='ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all'></i>
-												<i className='ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all'></i>
-												<i className='ease relative block h-0.5 rounded-sm bg-white transition-all'></i>
-											</div>
-										</a>
-									</li>
-									<li className='flex items-center px-4'>
-										<a
-											href='javascript:;'
-											className='p-0 text-sm text-white transition-all ease-nav-brand'
-										>
-											<i fixed-plugin-button-nav className='cursor-pointer fa fa-cog'></i>
-											{/* <!-- fixed-plugin-button-nav  --> */}
-										</a>
-									</li>
-								</ul>
 							</div>
 						</div>
 					</nav>
@@ -531,172 +501,6 @@ const DashBoard = () => {
 					</div>
 					{/* <!-- end cards --> */}
 				</main>
-
-				<div fixed-plugin>
-					<a
-						fixed-plugin-button
-						className='fixed px-4 py-2 text-xl bg-white shadow-lg cursor-pointer bottom-8 right-8 z-990 rounded-circle text-slate-700'
-					>
-						<i className='py-2 pointer-events-none fa fa-cog'> </i>
-					</a>
-					{/* <!-- -right-90 in loc de 0--> */}
-					<div
-						fixed-plugin-card
-						className='z-sticky backdrop-blur-2xl backdrop-saturate-200 dark:bg-slate-850/80 shadow-3xl w-90 ease -right-90 fixed top-0 left-auto flex h-full min-w-0 flex-col break-words rounded-none border-0 bg-white/80 bg-clip-border px-2.5 duration-200'
-					>
-						<div className='px-6 pt-4 pb-0 mb-0 border-b-0 rounded-t-2xl'>
-							<div className='float-left'>
-								<h5 className='mt-4 mb-0 dark:text-white'>Configuración</h5>
-								<p className='dark:text-white dark:opacity-80'>
-									See our dashboard options.
-								</p>
-							</div>
-							<div className='float-right mt-6'>
-								<button
-									fixed-plugin-close-button
-									className='inline-block p-0 mb-4 text-sm font-bold leading-normal text-center uppercase align-middle transition-all ease-in bg-transparent border-0 rounded-lg shadow-none cursor-pointer hover:-translate-y-px tracking-tight-rem bg-150 bg-x-25 active:opacity-85 dark:text-white text-slate-700'
-								>
-									<i className='fa fa-close'></i>
-								</button>
-							</div>
-							{/* <!-- End Toggle Button --> */}
-						</div>
-						<hr className='h-px mx-0 my-1 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent' />
-						<div className='flex-auto p-6 pt-0 overflow-auto sm:pt-4'>
-							{/* <!-- Sidebar Backgrounds --> */}
-							<div>
-								<h6 className='mb-0 dark:text-white'>Sidebar Colors</h6>
-							</div>
-							<a href='javascript:void(0)'>
-								<div className='my-2 text-left' sidenav-colors>
-									<span
-										className='py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-blue-500 to-violet-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-slate-700 text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700'
-										active-color
-										data-color='blue'
-										onclick='sidebarColor(this)'
-									></span>
-									<span
-										className='py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700'
-										data-color='gray'
-										onclick='sidebarColor(this)'
-									></span>
-									<span
-										className='py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-blue-700 to-cyan-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700'
-										data-color='cyan'
-										onclick='sidebarColor(this)'
-									></span>
-									<span
-										className='py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-emerald-500 to-teal-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700'
-										data-color='emerald'
-										onclick='sidebarColor(this)'
-									></span>
-									<span
-										className='py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-orange-500 to-yellow-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700'
-										data-color='orange'
-										onclick='sidebarColor(this)'
-									></span>
-									<span
-										className='py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-red-600 to-orange-600 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700'
-										data-color='red'
-										onclick='sidebarColor(this)'
-									></span>
-								</div>
-							</a>
-							{/* <!-- Sidenav Type --> */}
-							<div className='mt-4'>
-								<h6 className='mb-0 dark:text-white'>Sidenav Type</h6>
-								<p className='text-sm leading-normal dark:text-white dark:opacity-80'>
-									Choose between 2 different sidenav types.
-								</p>
-							</div>
-							<div className='flex'>
-								<button
-									transparent-style-btn
-									className='inline-block w-full px-4 py-2.5 mb-2 font-bold leading-normal text-center text-white capitalize align-middle transition-all bg-blue-500 border border-transparent border-solid rounded-lg cursor-pointer text-sm xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-to-tl xl-max:from-blue-500 xl-max:to-violet-500 xl-max:text-white xl-max:border-0 hover:-translate-y-px dark:cursor-not-allowed dark:opacity-65 dark:pointer-events-none dark:bg-gradient-to-tl dark:from-blue-500 dark:to-violet-500 dark:text-white dark:border-0 hover:shadow-xs active:opacity-85 ease-in tracking-tight-rem shadow-md bg-150 bg-x-25 bg-gradient-to-tl from-blue-500 to-violet-500 hover:border-blue-500'
-									data-className='bg-transparent'
-									active-style
-								>
-									White
-								</button>
-								<button
-									white-style-btn
-									className='inline-block w-full px-4 py-2.5 mb-2 ml-2 font-bold leading-normal text-center text-blue-500 capitalize align-middle transition-all bg-transparent border border-blue-500 border-solid rounded-lg cursor-pointer text-sm xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-to-tl xl-max:from-blue-500 xl-max:to-violet-500 xl-max:text-white xl-max:border-0 hover:-translate-y-px dark:cursor-not-allowed dark:opacity-65 dark:pointer-events-none dark:bg-gradient-to-tl dark:from-blue-500 dark:to-violet-500 dark:text-white dark:border-0 hover:shadow-xs active:opacity-85 ease-in tracking-tight-rem shadow-md bg-150 bg-x-25 bg-none hover:border-blue-500'
-									data-className='bg-white'
-								>
-									Dark
-								</button>
-							</div>
-							<p className='block mt-2 text-sm leading-normal dark:text-white dark:opacity-80 xl:hidden'>
-								You can change the sidenav type just on desktop view.
-							</p>
-							{/* <!-- Navbar Fixed --> */}
-							<div className='flex my-4'>
-								<h6 className='mb-0 dark:text-white'>Navbar Fixed</h6>
-								<div className='block pl-0 ml-auto min-h-6'>
-									<input
-										navbarFixed
-										className="rounded-10 duration-250 ease-in-out after:rounded-circle after:shadow-2xl after:duration-250 checked:after:translate-x-5.3 h-5 relative float-left mt-1 ml-auto w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-blue-500/95 checked:bg-blue-500/95 checked:bg-none checked:bg-right"
-										type='checkbox'
-									/>
-								</div>
-							</div>
-							<hr className='h-px my-6 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent ' />
-							<div className='flex mt-2 mb-12'>
-								<h6 className='mb-0 dark:text-white'>Light / Dark</h6>
-								<div className='block pl-0 ml-auto min-h-6'>
-									<input
-										dark-toggle
-										className="rounded-10 duration-250 ease-in-out after:rounded-circle after:shadow-2xl after:duration-250 checked:after:translate-x-5.3 h-5 relative float-left mt-1 ml-auto w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-blue-500/95 checked:bg-blue-500/95 checked:bg-none checked:bg-right"
-										type='checkbox'
-									/>
-								</div>
-							</div>
-							<a
-								target='_blank'
-								className='dark:border dark:border-solid dark:border-white inline-block w-full px-6 py-2.5 mb-4 font-bold leading-normal text-center text-white align-middle transition-all bg-transparent border border-solid border-transparent rounded-lg cursor-pointer text-sm ease-in hover:shadow-xs hover:-translate-y-px active:opacity-85 tracking-tight-rem shadow-md bg-150 bg-x-25 bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850'
-								href='https://www.creative-tim.com/product/argon-dashboard-tailwind'
-							>
-								Free Download
-							</a>
-							<a
-								target='_blank'
-								className='dark:border dark:border-solid dark:border-white dark:text-white inline-block w-full px-6 py-2.5 mb-4 font-bold leading-normal text-center align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer active:shadow-xs hover:-translate-y-px active:opacity-85 text-sm ease-in tracking-tight-rem bg-150 bg-x-25 border-slate-700 text-slate-700 hover:bg-transparent hover:text-slate-700 hover:shadow-none active:bg-slate-700 active:text-white active:hover:bg-transparent active:hover:text-slate-700 active:hover:shadow-none'
-								href='https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/argon-dashboard/'
-							>
-								View documentation
-							</a>
-							<div className='w-full text-center'>
-								<a
-									className='github-button'
-									href='https://github.com/creativetimofficial/argon-dashboard-tailwind'
-									data-icon='octicon-star'
-									data-size='large'
-									data-show-count='true'
-									aria-label='Star creativetimofficial/argon-dashboard on GitHub'
-								>
-									Star
-								</a>
-								<h6 className='mt-4 dark:text-white'>Thank you for sharing!</h6>
-								<a
-									href='https://twitter.com/intent/tweet?text=Check%20Argon%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23tailwindcss&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fargon-dashboard-tailwind'
-									className='inline-block px-5 py-2.5 mb-0 mr-2 font-bold text-center text-white align-middle transition-all border-0  rounded-lg cursor-pointer hover:shadow-xs hover:-translate-y-px active:opacity-85 leading-normal text-sm ease-in tracking-tight-rem shadow-md bg-150 bg-x-25 me-2 border-slate-700 bg-slate-700'
-									target='_blank'
-								>
-									{' '}
-									<i className='mr-1 fab fa-twitter'></i> Tweet{' '}
-								</a>
-								<a
-									href='https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/argon-dashboard-tailwind'
-									className='inline-block px-5 py-2.5 mb-0 mr-2 font-bold text-center text-white align-middle transition-all border-0  rounded-lg cursor-pointer hover:shadow-xs hover:-translate-y-px active:opacity-85 leading-normal text-sm ease-in tracking-tight-rem shadow-md bg-150 bg-x-25 me-2 border-slate-700 bg-slate-700'
-									target='_blank'
-								>
-									{' '}
-									<i className='mr-1 fab fa-facebook-square'></i> Share{' '}
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
 			</body>
 		</div>
 	);
--- client/src/components/DashBoard/pages/ClassCalendar.jsx
+++ client/src/components/DashBoard/pages/ClassCalendar.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SideNav from '../SideNav';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteBlog } from '../../../redux/actions/actions';
 
-const Publications = () => {
-	const { initial_posts, ig_posts } = useSelector((s) => s);
-
+const ClasesCalendar = () => {
 	return (
 		<div>
 			<body className='m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500'>
@@ -30,10 +26,10 @@ const Publications = () => {
 										className="text-sm pl-2 capitalize leading-normal text-white before:float-left before:pr-2 before:text-white before:content-['/']"
 										aria-current='page'
 									>
-										Publicaciones
+										Calendario
 									</li>
 								</ol>
-								<h6 className='mb-0 font-bold text-white capitalize'>Publicaciones</h6>
+								<h6 className='mb-0 font-bold text-white capitalize'>Calendario</h6>
 							</nav>
 
 							<div className='flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto'>
@@ -59,14 +55,12 @@ const Publications = () => {
 								<div className='relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border'>
 									<div className=' grid grid-cols-2 p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent'>
 										<div className=''>
-											<h6 className='dark:text-white'>Tabla de Publicaciones</h6>
+											<h6 className='dark:text-white'>Calendario</h6>
 										</div>
 										<div className='grid justify-end '>
-											<Link to='/blog/create'>
-												<button className='inline-block w-fit  py-2 px-4 text-center mb-0 font-bold text-white capitalize shadow-sm fill-current bg-blue-500 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 rounded-xl'>
-													Crear Publicación
-												</button>
-											</Link>
+											<button className='inline-block w-fit  py-2 px-4 text-center mb-0 font-bold text-white capitalize shadow-sm fill-current bg-blue-500 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 rounded-xl'>
+												Crear Clase
+											</button>
 										</div>
 									</div>
 									<div className='p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent'></div>
@@ -75,26 +69,21 @@ const Publications = () => {
 											<table className='items-center justify-center w-full mb-0 align-top border-collapse dark:border-white/40 text-slate-500'>
 												<thead className='align-bottom'>
 													<tr>
-														<th className='px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70'>
-															Tipo de publicación
+														<th className='px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70'>
+															Actividad
 														</th>
 														<th className='px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70'>
-															Titulo
-														</th>
-														<th className='px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70'>
-															Tag's
+															Fecha
 														</th>
 														<th className='px-6 py-3 pl-2 font-bold text-center uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70'>
-															Calificación
+															Hora
 														</th>
 														<th className='px-6 py-3 font-semibold capitalize align-middle bg-transparent border-b border-solid shadow-none dark:border-white/40 dark:text-white tracking-none whitespace-nowrap'></th>
 													</tr>
 												</thead>
-												<tbody className='border-t'>
-													{[...initial_posts, ...ig_posts]?.map((post) => {
-														return <SinglePublication post={post} />;
-													})}
-												</tbody>
+
+												{/* ALL CLASSES */}
+												<tbody className='border-t'>MAPEAR AQUI</tbody>
 											</table>
 										</div>
 									</div>
@@ -107,88 +96,3 @@ const Publications = () => {
 		</div>
 	);
 };
-
-const SinglePublication = ({ post }) => {
-	const dispatch = useDispatch();
-
-	const handleDelete = () => {
-		dispatch(deleteBlog(post.id));
-	};
-
-	return (
-		<tr>
-			{/* type of publication */}
-			<td className='p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<div className='flex px-2'>
-					<div>
-						<img
-							src={
-								post?.isInstagram
-									? 'https://res.cloudinary.com/dpxucxgwg/image/upload/v1679196395/instagram_pozow5.png'
-									: post?.image
-							}
-							className='inline-flex items-center justify-center mr-2 text-sm text-white transition-all duration-200 ease-in-out rounded-full h-8 w-8'
-							alt='post img'
-						/>
-					</div>
-					<div className='my-auto'>
-						<h6 className='mb-0 text-sm leading-normal dark:text-white'>
-							{post?.isInstagram ? 'Instagram' : 'Blog'}
-						</h6>
-					</div>
-				</div>
-			</td>
-
-			{/* title */}
-			<td className='p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<p className='mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60 capitalize'>
-					{post?.title}
-				</p>
-			</td>
-
-			{/* tag */}
-			<td className='p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<span className='text-xs font-semibold leading-tight dark:text-white dark:opacity-60'>
-					{post?.tag?.map((el, index) => {
-						if (index === post?.tag?.length - 1) return el;
-						else return el + '\n';
-					})}
-				</span>
-			</td>
-
-			{/* qualification*/}
-			<td className='p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<div className='flex items-center justify-center'>
-					<span className='mr-2 text-xs font-semibold leading-tight dark:text-white dark:opacity-60'>
-						60%
-					</span>
-					<div>
-						<div className='text-xs h-0.75 w-30 m-0 flex overflow-visible rounded-lg bg-gray-200'>
-							<div
-								className='flex flex-col justify-center w-3/5 h-auto overflow-hidden text-center text-white transition-all bg-blue-500 rounded duration-600 ease bg-gradient-to-tl from-blue-700 to-cyan-500 whitespace-nowrap'
-								role='progressbar'
-								aria-valuenow='60'
-								aria-valuemin='0'
-								aria-valuemax='100'
-							></div>
-						</div>
-					</div>
-				</div>
-			</td>
-
-			{/* delete btn */}
-			<td className='p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<button className='inline-block px-5 py-2.5 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none leading-normal text-sm ease-in bg-150 tracking-tight-rem bg-x-25 text-slate-400'>
-					<i
-						className='fa fa-times mr-4 text-red-500'
-						aria-hidden='true'
-						onClick={handleDelete}
-					></i>
-					{/* <i className="text-xs leading-tight fa fa-ellipsis-v dark:text-white dark:opacity-60"></i> */}
-				</button>
-			</td>
-		</tr>
-	);
-};
-
-export default Publications;
