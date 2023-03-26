26323769a768d7c38a3d9f03e9c5439fe6184633
nicovon24 <nicovon24@gmail.com>
2023-03-26 14:45:54 -0300

    fix

--- api/src/db.js
+++ api/src/db.js
@@ -18,19 +18,19 @@ const {
 
 // 5432
 // const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-// const { DB_DEPLOY } = process.env;
+const { DB_DEPLOY } = process.env;
 
-const db = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-	{
-		logging: false, // set to console.log to see the raw SQL queries
-		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-	}
-);
+// const db = new Sequelize(
+// 	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+// 	{
+// 		logging: false, // set to console.log to see the raw SQL queries
+// 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// 	}
+// );
 
-// const db = new Sequelize(DB_DEPLOY, { logging: false, native: false });
+const db = new Sequelize(DB_DEPLOY, { logging: false, native: false });
 
 Activities(db);
 Admin(db);
