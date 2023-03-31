a3af60b0d8ccd2dc83e6b78439275db37e198829
MrTban <esteman214@gmail.com>
2023-03-30 19:19:05 -0500

    correction of validations for forms

--- client/src/components/CreateReview/ValidationsRvw.js
+++ client/src/components/CreateReview/ValidationsRvw.js
@@ -1,16 +1,18 @@
 const regexName = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{2,50}$/;
 
-const ValidationsRvw = () => {
+const ValidationsRvw = (input) => {
 	let errors = {};
 
 	if (!input.name) {
 		errors.name = 'Debe incluir un Nombre';
 	} else if (!regexName.test(input.name)) {
 		errors.name = 'El Nombre contiene algún caracter invalido';
-	} else if (!input.text) {
-		errors.text = 'Debe agregar una Opinión';
-	} else if (input.text.length > 300) {
-		errors.text = 'La Opinión es demasiado larga, Max 300 caracteres.';
+	} else if (!input.rate) {
+		errors.rate = 'Debe incluir una Calificación';
+	} else if (!input.review) {
+		errors.review = 'Debe agregar una Opinión';
+	} else if (input.review.length > 300) {
+		errors.review = 'La Opinión es demasiado larga, Max 300 caracteres.';
 	}
 
 	return errors;
