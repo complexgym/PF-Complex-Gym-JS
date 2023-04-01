5727001ef66dd831da82e0710b0abc9bbf68ca33
MrTban <esteman214@gmail.com>
2023-04-01 13:04:37 -0500

    text center (lo que no puedo hacer mauri 🤣)

--- client/src/components/DashBoard/pages/cards/ClassCard.jsx
+++ client/src/components/DashBoard/pages/cards/ClassCard.jsx
@@ -56,10 +56,10 @@ const ClassCard = ({ calendar }) => {
 
 	return (
 		<tr>
-			<td className='w-48 p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<p className='w-48 text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
+			<td className='p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<p className='text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
 					<input
-						className={`border-none font-normal w-48 text-center ${
+						className={`border-none font-normal w-full text-center ${
 							!editable && 'text-gray-500'
 						}`}
 						name='classes'
@@ -70,10 +70,10 @@ const ClassCard = ({ calendar }) => {
 					></input>
 				</p>
 			</td>
-			<td className='w-48 p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<p className='w-48 text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
+			<td className='p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<p className='text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
 					<input
-						className={`border-none font-normal w-48 text-center ${
+						className={`border-none font-normal w-full text-center ${
 							!editable && 'text-gray-500'
 						}`}
 						name='day'
@@ -84,10 +84,10 @@ const ClassCard = ({ calendar }) => {
 					></input>
 				</p>
 			</td>
-			<td className='w-48 p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<p className='w-48 text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
+			<td className='align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<p className='text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
 					<input
-						className={`border-none font-normal w-48 text-center ${
+						className={`border-none font-normal w-full text-center ${
 							!editable && 'text-gray-500'
 						}`}
 						name='month'
@@ -98,10 +98,10 @@ const ClassCard = ({ calendar }) => {
 					></input>
 				</p>
 			</td>
-			<td className='w-48 p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<p className='w-48 text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
+			<td className='align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<p className='text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
 					<input
-						className={`border-none font-normal w-48 text-center ${
+						className={`border-none font-normal w-full text-center ${
 							!editable && 'text-gray-500'
 						}`}
 						name='year'
@@ -113,10 +113,10 @@ const ClassCard = ({ calendar }) => {
 				</p>
 			</td>
 
-			<td className='w-48 p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<p className='w-48 text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
+			<td className='align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<p className='text-xs font-semibold leading-tight dark:text-white dark:opacity-80'>
 					<input
-						className={`border-none font-normal w-48 text-center ${
+						className={`border-none font-normal w-full text-center ${
 							!editable && 'text-gray-500'
 						}`}
 						name='hour'
@@ -129,16 +129,16 @@ const ClassCard = ({ calendar }) => {
 			</td>
 
 			{/* edit */}
-			<td className='p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent text-center'>
+			<td className='align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent '>
 				{editable ? (
 					<i
-						className='fa fa-check w-8 cursor-pointer mr-2 text-green-600'
+						className='fa fa-check cursor-pointer text-green-600 w-full text-center'
 						aria-hidden='true'
 						onClick={() => setEditable(false)}
 					></i>
 				) : (
 					<i
-						className='fa fa-times text-sm cursor-pointer w-8 mr-2 text-red-500'
+						className='fa fa-times text-sm cursor-pointer text-red-500 w-full text-center'
 						aria-hidden='true'
 						onClick={() => setEditable(true)}
 					></i>
@@ -146,10 +146,10 @@ const ClassCard = ({ calendar }) => {
 			</td>
 
 			{/* send */}
-			<td className='p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent text-center'>
-				<button disabled={editable === false} onClick={handleClick}>
+			<td className='align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent '>
+				<button className='w-full' disabled={editable === false} onClick={handleClick}>
 					<i
-						className={`fa fa-paper-plane text-sm cursor-pointer ${
+						className={`fa fa-paper-plane text-sm cursor-pointer w-full text-center${
 							!editable && 'text-gray-500 cursor-auto'
 						}`}
 						aria-hidden='true'
@@ -157,11 +157,11 @@ const ClassCard = ({ calendar }) => {
 				</button>
 			</td>
 
-			<td className='px-2 py-3 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
-				<button className='inline-block px-5 py-2.5 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none leading-normal text-sm ease-in bg-150 tracking-tight-rem bg-x-25 text-slate-400'>
+			<td className='align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<button className='w-full inline-block font-bold  uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none leading-normal text-sm ease-in bg-150 tracking-tight-rem bg-x-25 text-slate-400'>
 					<img
 						src='https://res.cloudinary.com/dpxucxgwg/image/upload/v1679368276/test_complex/gas3ewhonfe4sqiqcqyy.png'
-						className='w-5'
+						className='w-6 m-auto text-center'
 						onClick={handleDelete}
 					/>
 					{/* <i className="text-xs leading-tight fa fa-ellipsis-v dark:text-white dark:opacity-60"></i> */}
