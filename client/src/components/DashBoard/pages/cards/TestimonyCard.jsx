a4dd045ac5ddb4bbc09a26dcbef4115052574e22
MrTban <esteman214@gmail.com>
2023-04-03 07:31:43 -0500

    Fav testimony changed

--- client/src/components/DashBoard/pages/cards/TestimonyCard.jsx
+++ client/src/components/DashBoard/pages/cards/TestimonyCard.jsx
@@ -1,76 +1,83 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { getAllTestimonials, putTestimonials } from "../../../../redux/actions/actions";
-import ReadMoreButton from "../../../ReadMoreButton/ReadMoreButton";
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getAllTestimonials, putTestimonials } from '../../../../redux/actions/actions';
+import ReadMoreButton from '../../../ReadMoreButton/ReadMoreButton';
 
 const TestimonyCard = ({ testimony }) => {
+	const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
+	const [data, setData] = useState({
+		url: testimony?.url,
+		name: testimony?.name,
+		review: testimony?.review,
+		fav: testimony?.fav,
+		rate: testimony?.rate,
+	});
 
-  const [data, setData] = useState({
-    url: testimony?.url,
-    name: testimony?.name,
-    review: testimony?.review,
-    fav: testimony?.fav,
-    rate: testimony?.rate,
-  });
+	const handleFav = (id) => {
+		const review = testimony;
+		setData({
+			...review,
+			fav: !review.fav,
+		});
+		dispatch(
+			putTestimonials(id, {
+				...review,
+				fav: !review.fav,
+			})
+		);
+		setTimeout(() => {
+			dispatch(getAllTestimonials());
+		}, 200);
+	};
 
-  const handleFav = (id) => {
-    const review = testimony
-    setData({
-      ...review,
-      fav: !review.fav,
-    });
-    dispatch(putTestimonials(id, {
-      ...review,
-      fav: !review.fav,
-    }));
-    setTimeout(() => {
-      dispatch(getAllTestimonials());
-    }, 200);
+	return (
+		<tr>
+			{/* IMAGE */}
+			<td className='px-2 py-3 pl-4 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<img
+					src={
+						testimony?.url
+							? testimony?.url
+							: 'https://res.cloudinary.com/dpxucxgwg/image/upload/v1679450694/anonimo_uim8xm.png'
+					}
+					className='inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-in-out h-9 w-9 rounded-xl'
+				/>
+			</td>
 
-  };
+			{/* NAME */}
+			<td className='px-2 py-3 pl-11 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<p className='mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60 capitalize'>
+					{testimony?.name}
+				</p>
+			</td>
 
-  return (
-    <tr>
-      {/* IMAGE */}
-      <td className="px-2 py-3 pl-4 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
-        <img
-          src={testimony?.url ? testimony?.url : "https://res.cloudinary.com/dpxucxgwg/image/upload/v1679450694/anonimo_uim8xm.png"}
-          className="inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-in-out h-9 w-9 rounded-xl"
-        />
-      </td>
+			{/* TEXT*/}
+			<td className='px-2 py-3 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<p className='mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60 capitalize'>
+					<ReadMoreButton text={testimony?.review} maxChars={20} />
+				</p>
+			</td>
 
-      {/* NAME */}
-      <td className="px-2 py-3 pl-11 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
-        <p className="mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60 capitalize">
-          {testimony?.name}
-        </p>
-      </td>
-
-      {/* TEXT*/}
-      <td className="px-2 py-3 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
-        <p className="mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60 capitalize">
-        <ReadMoreButton text={testimony?.review} maxChars={20} />
-        </p>
-      </td>
-
-      {/* FAV */}
-      <td className="px-2 py-3 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
-        <button
-          className={
-            testimony?.fav
-              ? "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  border-black border-2"
-              : "bg-[#fcfcfc] hover:bg-[#8c8b8b] hover:text-black text-grey font-bold py-2 px-4 rounded border-[#8c8b8b] border-2"
-          }
-          onClick={() => handleFav(testimony.id)}
-        >
-          {testimony?.fav ? "SI" : "NO"}
-        </button>
-      </td>
-    </tr>
-  );
-}
+			{/* FAV */}
+			<td className='px-2 py-3 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+				<button className='w-full' onClick={() => handleFav(testimony.id)}>
+					{testimony?.fav ? (
+						<i
+							className='fa fa-check w-8 cursor-pointer text-green-600'
+							aria-hidden='true'
+						></i>
+					) : (
+						<i
+							className='fa fa-times cursor-pointer w-8 text-red-500'
+							aria-hidden='true'
+						></i>
+					)}
+				</button>
+			</td>
+		</tr>
+	);
+};
 
 export default TestimonyCard;
 //
