1b8d9a139b4fb11f18dcfb1266de7cf53ee9f18f
MrTban <esteman214@gmail.com>
2023-04-03 07:39:26 -0500

    testimonials text center

--- client/src/components/DashBoard/pages/cards/TestimonyCard.jsx
+++ client/src/components/DashBoard/pages/cards/TestimonyCard.jsx
@@ -34,26 +34,26 @@ const TestimonyCard = ({ testimony }) => {
 	return (
 		<tr>
 			{/* IMAGE */}
-			<td className='px-2 py-3 pl-4 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+			<td className='text-center px-2 py-3 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
 				<img
 					src={
 						testimony?.url
 							? testimony?.url
 							: 'https://res.cloudinary.com/dpxucxgwg/image/upload/v1679450694/anonimo_uim8xm.png'
 					}
-					className='inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-in-out h-9 w-9 rounded-xl'
+					className='inline-flex items-center justify-center text-sm text-white transition-all duration-200 ease-in-out h-9 w-9 rounded-xl'
 				/>
 			</td>
 
 			{/* NAME */}
-			<td className='px-2 py-3 pl-11 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+			<td className=' text-center px-2 py-3 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
 				<p className='mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60 capitalize'>
 					{testimony?.name}
 				</p>
 			</td>
 
 			{/* TEXT*/}
-			<td className='px-2 py-3 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
+			<td className='text-center px-2 py-3 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent'>
 				<p className='mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60 capitalize'>
 					<ReadMoreButton text={testimony?.review} maxChars={20} />
 				</p>
